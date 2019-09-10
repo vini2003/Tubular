207771914e86bc670142b1fe5a29dfcc85c9f0f6
vini2003 <vini.sch@outlook.com>
2019-09-10 18:14:11 -0300

    Warning added for BlockState loading times.

--- src/main/java/tubular/TubularMod.java
+++ src/main/java/tubular/TubularMod.java
@@ -1,5 +1,11 @@
 package tubular;
 
+
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.fabricmc.api.ModInitializer;
 import tubular.registry.BlockRegistry;
 import tubular.registry.EntityRegistry;
@@ -10,6 +16,9 @@ import tubular.registry.ScreenServerRegistry;
 public class TubularMod implements ModInitializer {
     @Override
     public void onInitialize() {
+        Logger logger = LogManager.getLogger();
+        logger.log(Level.WARN, "[Tubular] I'm doing a very bad thing, the game is still loading, don't worry.");
+
         BlockRegistry.registerBlocks();
         ItemRegistry.registerItems();
         EntityRegistry.registerEntities();
